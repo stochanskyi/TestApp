a1eb7d8885c7f61d5f8e5761cde70ad42190d9fc
Marian Stochanskyi <stochan.andr@gmail.com>
2021-09-02 00:40:43 +0300

    Commit 2 feature3

--- app/src/main/java/com/mars/myapplication/MainActivity.kt
+++ app/src/main/java/com/mars/myapplication/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setConteasdntView(R.layout.activity_main)
+        sesdatContentakjsfdjiew(R.layout.activity_main)
     }
 
 }
